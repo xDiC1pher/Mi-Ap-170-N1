16f3b2076b2e25ac9d8e60359be62543125d936b
xDiC1pher <josi.gonza.ley@gmail.com>
2025-05-13 10:51:03 -0400

    commit

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
+from version.version import version
+
 # Proyecto : Control de inventario de un almacen
 # Autor: Manuel Sánchez Cárcamo
-"""
-Un método de versionamiento del codigo es el sgte
-Version MAJOR.MINOR.PATCH
-
-1.- MAJOR: (version mayor) se incrementa cuando se hacen ca,bios grandes o
-        incompatibles con version anterior, p.e.: cambio a base de datos, 
-        integracion de nuevas funciones/capacidades
-    MINOR (version menor): Se incrementa cuando se agregan funcionalidades nuevas
-        pero sin romper la compatibilidad
-    PATCH (parche o revision): Se increment cuando se corrigen errores o se hacen 
-        mejoras. p.e.: Agregan validaciones a la lectura de datos.
-
-"""
-
-# Historial
-#   2025.04.17  : Inicio del proyecto (v1.0.0)
-#   2025.04.22  : Agrega primeras opciones 1,2 y 3 (v1.1.0)
-#   2025.04.29  : cambia paradigma de desarrollo y pasamos de prog. lineal a modular v2.0.0
-#                 primera funcio validar_numero
-#   2025.05.06  : Se reemplaza acciones repetidas por funciones v2.0.1
 
 def valida_numero(mensaje="Valor : "):
     """
@@ -97,7 +79,6 @@ def fn_buscar_producto(productos, nom):
         return( -1 )
 
 #Programa Principal
-version = "v2.0.1"
 lnombre = ['Plumon', 'Borrador', 'Pizarra']
 lprecio = [1850.0, 3500.0, 13500.0]
 lstock = [20, 5, 10]
---
+++ version/version.py
@@ -0,0 +1,24 @@
+"""
+Un método de versionamiento del codigo es el sgte
+Version MAJOR.MINOR.PATCH
+
+1.- MAJOR: (version mayor) se incrementa cuando se hacen ca,bios grandes o
+        incompatibles con version anterior, p.e.: cambio a base de datos, 
+        integracion de nuevas funciones/capacidades
+    MINOR (version menor): Se incrementa cuando se agregan funcionalidades nuevas
+        pero sin romper la compatibilidad
+    PATCH (parche o revision): Se increment cuando se corrigen errores o se hacen 
+        mejoras. p.e.: Agregan validaciones a la lectura de datos.
+
+"""
+# Historial
+#   2025.04.17  : Inicio del proyecto (v1.0.0)
+#   2025.04.22  : Agrega primeras opciones 1,2 y 3 (v1.1.0)
+#   2025.04.29  : cambia paradigma de desarrollo y pasamos de prog. lineal a modular v2.0.0
+#                 primera funcio validar_numero
+#   2025.05.06  : Se reemplaza acciones repetidas por funciones v2.0.1
+#   2025.05.13  : Corregida importación de número de versión
+
+#   version = "v1.0.0"
+#   version = "v2.0.0"
+version = "v2.1.1"
